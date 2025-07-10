a11871c4d6e8a8921aa3b7cde3c0e40f593813e1
Owiks <osok.vadim@gmail.com>
2025-07-10 15:27:20 +0300

    Update mod_frozen_nick.lua

--- mod_frozen_nick.lua
+++ mod_frozen_nick.lua
@@ -8,49 +8,49 @@ local function on_presence(event)
     local stanza = event.stanza;
 
     if not origin then
-        log("debug", "Presence ignored: event.origin is nil");
+        log("info", "Presence ignored: event.origin is nil");
         return;
     end
 
     if not stanza then
-        log("debug", "[%s] Presence ignored: event.stanza is nil", origin.username or "unknown");
+        log("info", "[%s] Presence ignored: event.stanza is nil", origin.username or "unknown");
         return;
     end
 
     local jid = origin.username or "unknown";
-    log("debug", "[%s] Presence received:\n%s", jid, tostring(stanza));
+    log("info", "[%s] Presence received:\n%s", jid, tostring(stanza));
 
     local user_ctx = origin.jitsi_meet_context_user;
     if not user_ctx then
-        log("debug", "[%s] No jitsi_meet_context_user in session. Skipping nick override.", jid);
+        log("info", "[%s] No jitsi_meet_context_user in session. Skipping nick override.", jid);
         return;
     end
 
     -- 🔍 логируем весь контекст
     local ok, ctx_dump = pcall(json.encode, user_ctx);
     if ok then
-        log("debug", "[%s] jitsi_meet_context_user: %s", jid, ctx_dump);
+        log("info", "[%s] jitsi_meet_context_user: %s", jid, ctx_dump);
     else
-        log("debug", "[%s] Failed to JSON encode jitsi_meet_context_user", jid);
+        log("info", "[%s] Failed to JSON encode jitsi_meet_context_user", jid);
     end
 
     if not user_ctx.name then
-        log("debug", "[%s] jitsi_meet_context_user.name is not set. Skipping nick override.", jid);
+        log("info", "[%s] jitsi_meet_context_user.name is not set. Skipping nick override.", jid);
         return;
     end
 
     local forced_nick = user_ctx.name;
-    log("debug", "[%s] Forcing nick to '%s' from JWT context", jid, forced_nick);
+    log("info", "[%s] Forcing nick to '%s' from JWT context", jid, forced_nick);
 
     -- лог всех старых ников
     for tag in stanza:childtags("nick", "http://jabber.org/protocol/nick") do
-        log("debug", "[%s] Found original nick: '%s'", jid, tag:get_text());
+        log("info", "[%s] Found original nick: '%s'", jid, tag:get_text());
     end
 
     -- удаляем все <nick>
     stanza:maptags(function(tag)
         if tag.name == "nick" and tag.attr.xmlns == "http://jabber.org/protocol/nick" then
-            log("debug", "[%s] Removing existing <nick>", jid);
+            log("info", "[%s] Removing existing <nick>", jid);
             return nil;
         end
         return tag;
@@ -59,8 +59,8 @@ local function on_presence(event)
     -- вставляем новый <nick>
     stanza:tag("nick", { xmlns = "http://jabber.org/protocol/nick" }):text(forced_nick):up();
 
-    log("debug", "[%s] Nick successfully set to '%s'", jid, forced_nick);
-    log("debug", "[%s] Modified stanza:\n%s", jid, tostring(stanza));
+    log("info", "[%s] Nick successfully set to '%s'", jid, forced_nick);
+    log("info", "[%s] Modified stanza:\n%s", jid, tostring(stanza));
 end
 
 module:hook("pre-presence/bare", on_presence);
