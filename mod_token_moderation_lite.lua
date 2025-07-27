9cce7b60b149a6e56c38de1bfcd9868799f7dd55
Owiks <osok.vadim@gmail.com>
2025-07-27 04:43:14 +0300

    Update mod_token_moderation_lite.lua

--- mod_token_moderation_lite.lua
+++ mod_token_moderation_lite.lua
@@ -1,67 +1,75 @@
 -- Token moderation
--- this module looks for a field on incoming JWT tokens called "moderator". 
--- If it is true the user is added to the room as a moderator, otherwise they are set to a normal user.
--- Note this may well break other affiliation based features like banning or login-based admins
+-- This module looks for a field on incoming JWT tokens called "moderator". 
+-- If it is true, the user is added to the room as a moderator, otherwise as a participant.
+-- WARNING: This may interfere with other affiliation-based features like banning or admin login.
+
 local log = module._log;
 local jid_bare = require "util.jid".bare;
 local json = require "cjson";
 local basexx = require "basexx";
-local um_is_admin = require "core.usermanager".is_admin;
-
-local function is_admin(jid)
-        return um_is_admin(jid, module.host);
-end
 
 log('info', 'Loaded token moderation plugin');
--- Hook into room creation to add this wrapper to every new room
+
+local function is_admin(jid)
+    return module:may("admin", jid);
+end
+
+-- Hook into room creation to wrap affiliation logic
 module:hook("muc-room-created", function(event)
-        log('info', 'room created, adding token moderation code');
-        local room = event.room;
-        local _handle_normal_presence = room.handle_normal_presence;
-        local _handle_first_presence = room.handle_first_presence;
-        -- Wrap presence handlers to set affiliations from token whenever a user joins
-        room.handle_normal_presence = function(thisRoom, origin, stanza)
-                local pres = _handle_normal_presence(thisRoom, origin, stanza);
-                setupAffiliation(thisRoom, origin, stanza);
-                return pres;
-        end;
-        room.handle_first_presence = function(thisRoom, origin, stanza)
-                local pres = _handle_first_presence(thisRoom, origin, stanza);
-                setupAffiliation(thisRoom, origin, stanza);
-                return pres;
-        end;
-        -- Wrap set affilaition to block anything but token setting owner (stop pesky auto-ownering)
-        local _set_affiliation = room.set_affiliation;
-        room.set_affiliation = function(room, actor, jid, affiliation, reason)
-                -- let this plugin do whatever it wants
-                if actor == "token_plugin" then
-                        return _set_affiliation(room, true, jid, affiliation, reason)
-                -- noone else can assign owner (in order to block prosody/jisti's built in moderation functionality
-                elseif affiliation == "owner" then
-                        return nil, "modify", "not-acceptable"
-                -- keep other affil stuff working as normal (hopefully, haven't needed to use/test any of it)
-                else
-                        return _set_affiliation(room, actor, jid, affiliation, reason);
-                end;
-        end;
+    log('info', 'room created, adding token moderation logic');
+    local room = event.room;
+
+    -- Save original methods
+    local _handle_normal_presence = room.handle_normal_presence;
+    local _handle_first_presence = room.handle_first_presence;
+    local _set_affiliation = room.set_affiliation;
+
+    -- Wrap presence handlers
+    room.handle_normal_presence = function(thisRoom, origin, stanza)
+        local pres = _handle_normal_presence(thisRoom, origin, stanza);
+        setupAffiliation(thisRoom, origin, stanza);
+        return pres;
+    end;
+    room.handle_first_presence = function(thisRoom, origin, stanza)
+        local pres = _handle_first_presence(thisRoom, origin, stanza);
+        setupAffiliation(thisRoom, origin, stanza);
+        return pres;
+    end;
+
+    -- Wrap set_affiliation to block external owner assignment
+    room.set_affiliation = function(room, actor, jid, affiliation, reason)
+        if actor == "token_plugin" then
+            return _set_affiliation(room, true, jid, affiliation, reason);
+        elseif affiliation == "owner" then
+            return nil, "modify", "not-acceptable";
+        else
+            return _set_affiliation(room, actor, jid, affiliation, reason);
+        end
+    end;
 end);
+
 function setupAffiliation(room, origin, stanza)
-        if origin.auth_token then
-                -- Extract token body and decode it
-                local dotFirst = origin.auth_token:find("%.");
-                if dotFirst then
-                        local dotSecond = origin.auth_token:sub(dotFirst + 1):find("%.");
-                        if dotSecond then
-                                local bodyB64 = origin.auth_token:sub(dotFirst + 1, dotFirst + dotSecond - 1);
-                                local body = json.decode(basexx.from_url64(bodyB64));
-                                local jid = jid_bare(stanza.attr.from);
-                                -- If user is a moderator or an admin, set their affiliation to be an owner
-                                if body["moderator"] == true or is_admin(jid) then
-                                        room:set_affiliation("token_plugin", jid, "owner");
-                                else
-                                        -- room:set_affiliation("token_plugin", jid, "member");
-                                end;
-			end;
-		end;
-	end;
-end;
+    if not origin or not origin.auth_token then return end
+
+    local token = origin.auth_token;
+    local dot1 = token:find("%.");
+    if not dot1 then return end
+
+    local dot2 = token:sub(dot1 + 1):find("%.");
+    if not dot2 then return end
+
+    local b64body = token:sub(dot1 + 1, dot1 + dot2 - 1);
+    local body_json = basexx.from_url64(b64body);
+    local ok, body = pcall(json.decode, body_json);
+    if not ok then
+        log("warn", "Failed to decode token body: %s", tostring(body));
+        return;
+    end
+
+    local jid = jid_bare(stanza.attr.from);
+    local moderator = body["moderator"];
+
+    if moderator == true or is_admin(jid) then
+        room:set_affiliation("token_plugin", jid, "owner");
+    end
+end
